92c496d609e1c25c4a0a17baf7e3a1986a6ee75b
Benjamin <benjamin.b@a-cti.com>
2017-02-10 16:54:37 +0530

    added svg icons

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,31 @@ var gulp      = require('gulp'),
     connect   = require('gulp-connect'),
     sass      = require('gulp-sass'),
     cssmin    = require('gulp-cssmin'),
-    rename    = require('gulp-rename');
+    rename    = require('gulp-rename'),
+    iconfont = require('gulp-iconfont'),
+    iconfontCss = require('gulp-iconfont-css');
+
+
+var fontname = 'svgicons';
+
+gulp.task('iconfont', function(){
+  gulp.src(['images/svg/*.svg'])
+  .pipe(iconfontCss({
+    fontName: fontname,
+    targetPath: '../css/svgicons.css',
+    fontPath: '../fonts/',
+    cssClass: 'aw-svg'
+  }))
+  .pipe(iconfont({
+    fontName: fontname,
+    formats: ['ttf', 'eot', 'woff', 'woff2'],
+    normalize: true,
+    fontHeight: 1000
+  }))
+  .pipe(gulp.dest('fonts/'))
+  .pipe(connect.reload());
+});
+
 
 gulp.task('webserver', function() {
     connect.server({
@@ -35,4 +59,4 @@ gulp.task('watch',function(){
   gulp.watch(['scripts/*.js'],['script']);
 });
 
-gulp.task('default',['sass','html','script', 'webserver','watch']);
+gulp.task('default',['sass','html','script', 'webserver','watch', 'iconfont']);
